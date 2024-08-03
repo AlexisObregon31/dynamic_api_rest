967dee519feeb0c90185c7db22cd0fb62ebad5b8
AlexisObregon31 <79345023+AlexisObregon31@users.noreply.github.com>
2024-08-03 09:41:19 -0400

    Update consultasSql.js

--- Servicios/apiWhatsapp/consultasSql.js
+++ Servicios/apiWhatsapp/consultasSql.js
@@ -5,8 +5,7 @@ const conectConfigUtilidades = {
   connectionTimeout: 60,
   loginTimeout: 60,
 };
-    //Hacer 3 funciones separadas para cada empresa para obtener los datos
-    //Caso sea el mismo mensaje y misma estructura se puede con uno solo al igual que CDE
+
  async function obtenerDatos(){
     try {
         conectarBDFirebird = await odbc.connect(conectConfigUtilidades);
